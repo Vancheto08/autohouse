fb7f6e82823880914707674e3e2e2d2ed63a8302
Vancheto08 <vanya084115@gmail.com>
2025-03-31 18:54:27 +0300

    Create AutohouseContext

--- Data/AutohouseContext.cs
+++ Data/AutohouseContext.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data
 {
-    internal class AutohouseContext
+    internal class AutohouseContext : DbContext
     {
+        public AutohouseContext():base("name=AutohouseContext")
+        {
+
+        }
+        public DbSet<Brand> Brands { get; set; }
     }
 }
