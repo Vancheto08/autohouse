5ef41829296fa43dfaec16edae9fa9a8bf21c496
NellyKass <nelykasabova@abv.bg>
2025-03-31 21:33:23 +0300

    carbusiness logic

--- Business/CarBusiness.cs
+++ Business/CarBusiness.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Data;
+using Data.Models;
 
 namespace Business
 {
     public class CarBusiness
     {
+        private AutohouseContext autohouseContext;
+
+        public List<Car> GetAll()
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Cars.ToList();
+            }
+
+        }
+
+        public Car Get(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Cars.Find(id);
+            }
+        }
+
+        public void Add(Car car)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                autohouseContext.Cars.Add(car);
+                autohouseContext.SaveChanges();
+            }
+        }
+
+        public void Update(Car car)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var item = autohouseContext.Cars.Find(car.CarId);
+                autohouseContext.Entry(item).CurrentValues.SetValues(car);
+                autohouseContext.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var car = autohouseContext.Cars.Find(id);
+                if (car != null)
+                {
+                    autohouseContext.Cars.Remove(car);
+                    autohouseContext.SaveChanges();
+                }
+            }
+        }
     }
 }
