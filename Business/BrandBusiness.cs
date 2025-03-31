1df4fd27a7b151b171fd9ce8862f26a530b40a77
Vancheto08 <vanya084115@gmail.com>
2025-03-31 21:32:52 +0300

    Creade CRUD operations for BrandBusiness

--- Business/BrandBusiness.cs
+++ Business/BrandBusiness.cs
@@ -4,6 +4,7 @@ using Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,8 +16,49 @@ namespace Business
         private AutohouseContext autohouseContext;
         public List<Brand> GetAll()
         {
-
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Brands.ToList();
+            }
         }
-
+        public Brand Get(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Brands.Find(id);
+            }
+        }
+        public void Add(Brand brand)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                autohouseContext.Brands.Add(brand);
+                autohouseContext.SaveChanges();
+            }
+        }
+        public void Update(Brand brand)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var item = autohouseContext.Brands.Find(brand.BrandId);
+                if (item != null)
+                {
+                    autohouseContext.Entry(item).CurrentValues.SetValues(brand);
+                    autohouseContext.SaveChanges();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var item = autohouseContext.Brands.Find(id);
+                if (item != null)
+                {
+                    autohouseContext.Brands.Remove(item);
+                    autohouseContext.SaveChanges();
+                }
+            }
+        }
     }
 }
