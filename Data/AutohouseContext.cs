72aec9c41d0ae4c5c6f76995f73eea31da57911b
Vancheto08 <vanya084115@gmail.com>
2025-03-31 21:38:18 +0300

    Create DbSets

--- Data/AutohouseContext.cs
+++ Data/AutohouseContext.cs
@@ -10,11 +10,15 @@ namespace Data
 {
     public class AutohouseContext : DbContext
     {
-        public AutohouseContext():base("name=AutohouseContext")
+        public AutohouseContext() : base("name=AutohouseContext")
         {
 
         }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Order_Car> Orders_Cars { get; set; }
     }
 }
