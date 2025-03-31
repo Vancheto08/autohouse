a8703360aa6a88576bcd3b3facd9b02092f5fd0e
Vancheto08 <vanya084115@gmail.com>
2025-03-31 21:58:17 +0300

    Create CRUD operations for CustomerBusiness

--- Business/CustomerBusiness.cs
+++ Business/CustomerBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,52 @@ namespace Business
 {
     public class CustomerBusiness
     {
+        private AutohouseContext autohouseContext;
+        public List<Customer> GetAll(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Customers.ToList();
+            }
+        }
+        public Customer Get(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                return autohouseContext.Customers.Find(id);
+            }
+        }
+        public void Add(Customer customer)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                autohouseContext.Customers.Add(customer);
+                autohouseContext.SaveChanges();
+            }
+        }
+        public void Update(Customer customer)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var item = autohouseContext.Customers.Find(customer.CustomerId);
+                if (item != null)
+                {
+                    autohouseContext.Entry(item).CurrentValues.SetValues(customer);
+                    autohouseContext.SaveChanges();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (autohouseContext = new AutohouseContext())
+            {
+                var item = autohouseContext.Customers.Find(id);
+                if (item != null)
+                {
+                    autohouseContext.Customers.Remove(item);
+                    autohouseContext.SaveChanges();
+                }
+            }
+        }
     }
 }
