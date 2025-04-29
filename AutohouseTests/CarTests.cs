383745dd250aee9ac66b6610272cd21efff4fe0b
Vancheto08 <vanya084115@gmail.com>
2025-04-29 21:48:17 +0300

    Make 3 tests for CarTests

--- AutohouseTests/CarTests.cs
+++ AutohouseTests/CarTests.cs
@@ -27,5 +27,55 @@ namespace AutohouseTests
             context = new AutohouseContext();
             carBusiness = new CarBusiness(context);
         }
+        [Test]
+        public void GetAllReturnsAllBrands()
+        {
+            //Arrange
+            var brand = new Brand { BrandId = 1, Name = "Brand1" };
+            context.Brands.Add(brand);
+            context.SaveChanges();
+
+            var car1 = new Car { CarId = 1, BrandId = 1, Brand = brand, Price = 10000, PublicationYear = 2020, Quantity = 5 };
+            var car2 = new Car { CarId = 2, BrandId = 1, Brand = brand, Price = 12000, PublicationYear = 2021, Quantity = 3 };
+            context.Cars.Add(car1);
+            context.Cars.Add(car2);
+            context.SaveChanges();
+            //Act
+            var result = carBusiness.GetAll();
+            //Assert
+            ClassicAssert.AreEqual(2, result.Count);
+        }
+        [Test]
+        public void GetReturnsCars()
+        {
+            //Arrange
+            var brand = new Brand { BrandId = 1, Name = "Brand1" };
+            context.Brands.Add(brand);
+            context.SaveChanges();
+
+            var car1 = new Car { CarId = 1, BrandId = 1, Brand = brand, Price = 10000, PublicationYear = 2020, Quantity = 5 };
+            var car2 = new Car { CarId = 2, BrandId = 1, Brand = brand, Price = 12000, PublicationYear = 2021, Quantity = 3 };
+            context.Cars.Add(car1);
+            context.Cars.Add(car2);
+            context.SaveChanges();
+            //Act
+            var result = carBusiness.Get(1);
+            //Assert
+            ClassicAssert.AreEqual(car1.CarId, result.CarId);
+        }
+        [Test]
+        public void TryAddcar()
+        {
+            //Arrange
+            var brand = new Brand { BrandId = 1, Name = "Brand1" };
+            context.Brands.Add(brand);
+            context.SaveChanges();
+            var car = new Car { CarId = 1, BrandId = 1, Brand = brand, Price = 10000, PublicationYear = 2020, Quantity = 5 };
+            //Act
+            var result = context.Cars.FirstOrDefault(c => c.CarId == car.CarId);
+            //Assert
+            ClassicAssert.NotNull(result);
+            ClassicAssert.AreEqual(car.CarId, result.CarId);
+        }
     }
 }
