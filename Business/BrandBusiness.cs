40677b641b8e98cc8374e0bb0c6171bab0a6830f
Vancheto08 <vanya084115@gmail.com>
2025-04-30 18:24:05 +0300

    Make XML for BrandBusiness

--- Business/BrandBusiness.cs
+++ Business/BrandBusiness.cs
@@ -12,46 +12,50 @@ namespace Business
     {
         private DbContext autohouseContext;
         /// <summary>
-        /// Create an instance of <see href="BrandBusiness">with the  specified context
+        /// Create an instance of <see href="BrandBusiness"> with the  specified context.
         /// </summary>
-        /// <param name="context">instance of DbContext  </param>
+        /// <param name="context">An instance of DbContext.</param>
         public BrandBusiness(DbContext context)
         {
             autohouseContext = context;
         }
         /// <summary>       
-        /// Create an instance of <see href="BrandBusiness">
+        /// Create an instance of <see href="BrandBusiness">.
         /// </summary>
         public BrandBusiness()
         {
             autohouseContext = new AutohouseContext();
         }
         /// <summary>
-        /// Get all brands
+        /// Gets all the brands from the database.
         /// </summary>
-        /// <returns>List with all Brands</returns>
+        /// <returns>A list of all brands.</returns>
         public List<Brand> GetAll()
         {
             return autohouseContext.Set<Brand>().ToList();
         }
         /// <summary>
-        /// Get brand by id
+        /// Get a brand by its Id.
         /// </summary>
-        /// <param name="id">ineger - id of Brand</param>
-        /// <returns>value of type Brand </returns>
+        /// <param name="id">The Id of the brand to retrieve.</param>
+        /// <returns>The brand with the specified Id.</returns>
         public Brand Get(int id)
         {
             return autohouseContext.Set<Brand>().Find(id);
         }
         /// <summary>
-        /// 
+        /// Adds a new brand to the database.
         /// </summary>
-        /// <param name="brand"></param>
+        /// <param name="brand">The brand to add.</param>
         public void Add(Brand brand)
         {
             autohouseContext.Set<Brand>().Add(brand);
             autohouseContext.SaveChanges();
         }
+        /// <summary>
+        /// Updates an existing brand in the database.
+        /// </summary>
+        /// <param name="brand">The brand with updated values.</param>
         public void Update(Brand brand)
         {
             var item = autohouseContext.Set<Brand>().Find(brand.BrandId);
@@ -61,6 +65,10 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
+        /// <summary>
+        /// Deletes a brand from the database by its Id.
+        /// </summary>
+        /// <param name="id">The Id of the brand to delete.</param>
         public void Delete(int id)
         {
             var brand = autohouseContext.Set<Brand>().Find(id);
