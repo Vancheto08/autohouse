35de30de9363b3c118afd4f492364ab30d3c0a20
Vancheto08 <vanya084115@gmail.com>
2025-04-30 18:40:51 +0300

    Make XML for ManufacturerBusiness

--- Business/ManufacturerBusiness.cs
+++ Business/ManufacturerBusiness.cs
@@ -9,30 +9,51 @@ namespace Business
     public class ManufacturerBusiness
     {
         private DbContext autohouseContext;
-
+        /// <summary>
+        /// Create an instance of <see href="ManufacturerBusiness"> with the specified context.
+        /// </summary>
+        /// <param name="context">An instance of DbContext.</param>
         public ManufacturerBusiness(DbContext context)
         {
             autohouseContext = context;
         }
+        /// <summary>
+        /// Create an instance of <see href="ManufacturerBusiness">.
+        /// </summary>
         public ManufacturerBusiness()
         {
             autohouseContext = new AutohouseContext();
         }
+        /// <summary>
+        /// Gets all the manufacturers from the database.
+        /// </summary>
+        /// <returns>A list of all manufacturers.</returns>
         public List<Manufacturer> GetAll()
         {
             return autohouseContext.Set<Manufacturer>().ToList();
         }
+        /// <summary>
+        /// Gets a manufacturer by its Id.
+        /// </summary>
+        /// <param name="id">The Id of the manufacturer to retrieve.</param>
+        /// <returns>The manufacturer with the specified Id.</returns>
         public Manufacturer Get(int id)
         {
             return autohouseContext.Set<Manufacturer>().Find(id);
         }
-
+        /// <summary>
+        /// Adds a new manufacturer to the database.
+        /// </summary>
+        /// <param name="manufacturer">The manufacturer to add.</param>
         public void Add(Manufacturer manufacturer)
         {
             autohouseContext.Set<Manufacturer>().Add(manufacturer);
             autohouseContext.SaveChanges();
         }
-
+        /// <summary>
+        /// Updates an existing manufacturer in the database.
+        /// </summary>
+        /// <param name="manufacturer">The manufacturer with updated values.</param>
         public void Update(Manufacturer manufacturer)
         {
             var item = autohouseContext.Set<Manufacturer>().Find(manufacturer.ManufacturerId);
@@ -42,6 +63,10 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
+        /// <summary>
+        /// Deletes a manufacturer from the database by its Id.
+        /// </summary>
+        /// <param name="id">The Id of the manufacturer to delete.</param>
         public void Delete(int id)
         {
             var item = autohouseContext.Set<Manufacturer>().Find(id);
@@ -51,6 +76,5 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
-
     }
 }
