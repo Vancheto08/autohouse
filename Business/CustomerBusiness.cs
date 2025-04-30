140bfc74c9e5c1ca4bfa01008ae628d9e2ac4ed8
Vancheto08 <vanya084115@gmail.com>
2025-04-30 18:35:16 +0300

    Make XML for CustomerBusiness

--- Business/CustomerBusiness.cs
+++ Business/CustomerBusiness.cs
@@ -9,27 +9,51 @@ namespace Business
     public class CustomerBusiness
     {
         private DbContext autohouseContext;
+        /// <summary>
+        /// Create an instance of <see href="CustomerBusiness"> with the specified context.
+        /// </summary>
+        /// <param name="context">An instance of DbContext.</param>
         public CustomerBusiness(DbContext context)
         {
             autohouseContext = context;
         }
+        /// <summary>
+        /// Create an instance of <see href="CustomerBusiness">.
+        /// </summary>
         public CustomerBusiness()
         {
             autohouseContext = new AutohouseContext();
         }
+        /// <summary>
+        /// Gets all the customers from the database.
+        /// </summary>
+        /// <returns>A list of all customers.</returns>
         public List<Customer> GetAll()
         {
             return autohouseContext.Set<Customer>().ToList();
         }
+        /// <summary>
+        /// Gets a customer by its Id.
+        /// </summary>
+        /// <param name="id">The Id of the customer to retrieve.</param>
+        /// <returns>The customer with the specified Id.</returns>
         public Customer Get(int id)
         {
             return autohouseContext.Set<Customer>().Find(id);
         }
+        /// <summary>
+        /// Adds a new customer to the database.
+        /// </summary>
+        /// <param name="customer">The customer to add.</param>
         public void Add(Customer customer)
         {
             autohouseContext.Set<Customer>().Add(customer);
             autohouseContext.SaveChanges();
         }
+        /// <summary>
+        /// Updates an existing customer in the database.
+        /// </summary>
+        /// <param name="customer">The customer with updated values.</param>
         public void Update(Customer customer)
         {
             var item = autohouseContext.Set<Customer>().Find(customer.CustomerId);
@@ -39,6 +63,10 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
+        /// <summary>
+        /// Deletes a customer from the database by its Id.
+        /// </summary>
+        /// <param name="id">The Id of the customer to delete.</param>
         public void Delete(int id)
         {
             var customer = autohouseContext.Set<Customer>().Find(id);
