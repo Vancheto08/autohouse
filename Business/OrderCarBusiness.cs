7b2a06716c5b11f32a2f21324b94fbad5100c7c2
Vancheto08 <vanya084115@gmail.com>
2025-04-30 19:11:29 +0300

    Make XML for OrderCarBusiness and QueryBusiness

--- Business/OrderCarBusiness.cs
+++ Business/OrderCarBusiness.cs
@@ -10,20 +10,33 @@ namespace Business
     public class OrderCarBusiness
     {
         private DbContext autohouseContext;
+        /// <summary>
+        /// Create an instance of <see href="OrderCarBusiness"> with the specified context.
+        /// </summary>
+        /// <param name="context">An instance of DbContext.</param>
         public OrderCarBusiness(DbContext context)
         {
             autohouseContext = context;
         }
+        /// <summary>
+        /// Create an instance of <see href="OrderCarBusiness">.
+        /// </summary>
         public OrderCarBusiness()
         {
             autohouseContext = new AutohouseContext();
         }
-
+        /// <summary>
+        /// Gets all order-cars from the database.
+        /// </summary>
+        /// <returns>A list of all order-cars.</returns>
         public List<Order_Car> GetAll()
         {
             return autohouseContext.Set<Order_Car>().ToList();
         }
-
+        /// <summary>
+        /// Gets a list of car brands with basic details from the database.
+        /// </summary>
+        /// <returns>A list of car brands with Id, name, price, year, and quantity.</returns>
         public List<CarBrand> GetAllCarBrand()
         {
             return autohouseContext.Set<Car>().Select(x => new CarBrand
@@ -35,19 +48,29 @@ namespace Business
                 Quantity = x.Quantity
             }).ToList();
         }
-
-
+        /// <summary>
+        /// Gets an order-car by order Id and car Id.
+        /// </summary>
+        /// <param name="orderId">The Id of the order.</param>
+        /// <param name="carId">The Id of the car.</param>
+        /// <returns>The order-car record that exists, if it doesn't - null.</returns>
         public Order_Car Get(int orderId, int carId)
         {
             return autohouseContext.Set<Order_Car>().Find(orderId, carId);
         }
-
+        /// <summary>
+        /// Adds a new order-car record to the database.
+        /// </summary>
+        /// <param name="order_Car">The order-car record to add.</param>
         public void Add(Order_Car order_Car)
         {
             autohouseContext.Set<Order_Car>().Add(order_Car);
             autohouseContext.SaveChanges();
         }
-
+        /// <summary>
+        /// Updates an existing order-car record in the database.
+        /// </summary>
+        /// <param name="order_Car">The order-car record with updated values.</param>
         public void Update(Order_Car order_Car)
         {
             var item = autohouseContext.Set<Order_Car>().Find(order_Car.OrderId, order_Car.CarId);
@@ -58,7 +81,11 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
-
+        /// <summary>
+        /// Deletes an order-car record from the database by order Id and car Id.
+        /// </summary>
+        /// <param name="orderId">The Id of the order.</param>
+        /// <param name="carId">The Id of the car.</param>
         public void Delete(int orderId, int carId)
         {
             var item = autohouseContext.Set<Order_Car>().Find(orderId, carId);
@@ -69,14 +96,31 @@ namespace Business
                 autohouseContext.SaveChanges();
             }
         }
-
     }
+    /// <summary>
+    /// Represents a car brand with its details such as Id, name, price, publication year, and quantity.
+    /// </summary>
     public class CarBrand
     {
+        /// <summary>
+        /// Gets or sets the Id of the car brand.
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the car brand.
+        /// </summary>
         public string BrandName { get; set; }
+        /// <summary>
+        /// Gets or sets the price of the car.
+        /// </summary>
         public decimal Price { get; set; }
+        /// <summary>
+        /// Gets or sets the publication year of the car.
+        /// </summary>
         public int PublicationYear { get; set; }
+        /// <summary>
+        /// Gets or sets the quantity of cars available for this brand.
+        /// </summary>
         public int Quantity { get; set; }
     }
 }
--- Business/QueryBusiness.cs
+++ Business/QueryBusiness.cs
@@ -9,6 +9,11 @@ namespace Business
     public class QueryBusiness
     {
         //1
+        /// <summary>
+        /// Retrieves a list of orders made by customers with the specified payment method ("с карта").
+        /// </summary>
+        /// <param name="paymentMethod">The payment method to filter orders by ("с карта").</param>
+        /// <returns>A collection of orders made with the specified payment method.</returns>
         public ICollection<Order> GetCardOrdersByCustomers(string paymentMethod = "с карта")
         {
             using (AutohouseContext context = new AutohouseContext())
@@ -18,8 +23,11 @@ namespace Business
                 return cardOrders;
             }
         }
-
         //2
+        /// <summary>
+        /// Retrieves the car that has been ordered the most from the database.
+        /// </summary>
+        /// <returns>The most ordered car, including its brand and manufacturer details. If it doesn't exist - null.</returns>
         public Car GetMostOrderedCar()
         {
             using (AutohouseContext context = new AutohouseContext())
@@ -44,10 +52,11 @@ namespace Business
                 return null;
             }
         }
-
-
-
         //3
+        /// <summary>
+        /// Retrieves the count of cars ordered by each manufacturer.
+        /// </summary>
+        /// <returns>A list of manufacturers with the count of cars ordered for each manufacturer.</returns>
         public List<ManufacturerCountCars> GetCarCountByManufacturer()
         {
             using (AutohouseContext context = new AutohouseContext())
@@ -65,6 +74,11 @@ namespace Business
             }
         }
         //4
+        /// <summary>
+        /// Retrieves a list of cars by a specific manufacturer name.
+        /// </summary>
+        /// <param name="manufacturerName">The name of the manufacturer to filter cars by.</param>
+        /// <returns>A collection of cars that belong to the specified manufacturer.</returns>
         public ICollection<Car> GetCarsByManufacturer(string manufacturerName)
         {
             using (AutohouseContext context = new AutohouseContext())
@@ -73,10 +87,18 @@ namespace Business
             }
         }
     }
-
+    /// <summary>
+    /// Represents the count of cars for a specific manufacturer.
+    /// </summary>
     public class ManufacturerCountCars
     {
+        /// <summary>
+        /// Gets or sets the name of the manufacturer.
+        /// </summary>
         public string ManufacturerName { get; set; }
+        /// <summary>
+        /// Gets or sets the number of cars ordered for this manufacturer.
+        /// </summary>
         public int CarCount { get; set; }
     }
 
